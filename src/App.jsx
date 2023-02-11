3f2ba77c01824d8c3849b7e876b0055164c721b2
leonardo2322 <joys23services@gmail.com>
2023-02-10 21:49:24 -0500

    acordeon agregado walpaper principal

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import './App.css'
 import NavbarP from './components/navbar.jsx'
 import Image from 'react-bootstrap/Image'
 import { Container } from 'react-bootstrap'
+import Acordions from './components/Acordion'
 function App() {
 
   return (
     <div className="App">
       <Container className='cont-img-portada d-flex ' >
-        <Image src={`src/assets/img/comida-portada.jpg`} alt='Portada'  fluid={true} className="portada"/>
+        <Image src={`src/assets/img/as.jpg`} alt='Portada'  fluid={true} className="portada"/>
         <div className='container'>
           <Image src={`src/assets/img/me.jpg`}  alt='perfil' fluid={true} className="ImgPerfil"/>
         </div>
       </Container>
-
-      <NavbarP></NavbarP>
+       <NavbarP></NavbarP>
+       <Container className='main'>
+       <Acordions />
+       </Container>
     </div>
   )
 }
---
+++ src/components/Acordion.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Accordion from 'react-bootstrap/Accordion'
+import '../styles/acordion.css'
+import {BsChevronDown }from "react-icons/bs"
+
+const Acordions = () => {
+  return (
+    <>
+    <Accordion>
+      <Accordion.Item eventKey="0" className="AcorItem1">
+        <Accordion.Header className="AcorH">Frutas <BsChevronDown /></Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header className="AcorH">Verduras <BsChevronDown /></Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header className="AcorH">Abarrotes <BsChevronDown /></Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+     </>
+  );
+};
+
+export default Acordions;

c17ae9f90df3c614044e613dabe35ff1548821cc
leonardo2322 <joys23services@gmail.com>
2023-02-13 12:16:36 -0500

    cards add

--- src/components/Acordion.jsx
+++ src/components/Acordion.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Accordion from 'react-bootstrap/Accordion'
 import '../styles/acordion.css'
 import {BsChevronDown }from "react-icons/bs"
+import Cards from "./cards";
 
 const Acordions = () => {
   return (
@@ -10,13 +11,7 @@ const Acordions = () => {
       <Accordion.Item eventKey="0" className="AcorItem1">
         <Accordion.Header className="AcorH">Frutas <BsChevronDown /></Accordion.Header>
         <Accordion.Body className="acorBody">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <Cards></Cards>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
-const cards = () => {
+import React from "react";
+import "../styles/cards.css";
+import Image from "react-bootstrap/Image";
+const Cards = () => {
   return (
-    <div >
+    <div>
+      <div className="container-cards">
+        <div className="card_box">
+          <div className="image">
+            <Image src="../img/Fresas.png" className="card-img"></Image>
+          </div>
+          <h2 className="title">fresas</h2>
+          <h3 className="price"><strong>$</strong><p>100</p></h3>
+          <div className="image">
+            <button className="btnAdd">+ Add</button>
+          </div>
 
+          <span></span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default cards
+export default Cards;

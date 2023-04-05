6aba020e2678ddd5a156bfbc0e6ae48a136c7318
leonardo2322 <joys23services@gmail.com>
2023-04-04 21:30:27 -0400

    carrito de compras estilizado

--- src/components/shoppinCart.jsx
+++ src/components/shoppinCart.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import "../styles/PopupCart.css";
-import { BsFillXCircleFill,BsTrash,BsPencilSquare } from "react-icons/bs";
+import { BsFillXCircleFill, BsTrash, BsPencilSquare } from "react-icons/bs";
 import ApiContext from "../contexts/ContextAPi";
 function ShoppinCart({ max, CloseSet }) {
   const { db } = useContext(ApiContext);
@@ -25,24 +25,40 @@ function ShoppinCart({ max, CloseSet }) {
             </div>
             <div id="bodyPopup">
               {db.map((car) => (
-                <div key={car.id} className="container-products">
+                <div
+                  key={car.id}
+                  className="container-products"
+                  data-id={car.id}
+                >
                   <img src={car.img} alt={car.nombre} />
                   <div className="cont-espe">
                     <h4>{car.nombre}</h4>
-                    <button type="button">-</button>
-                    <h5>
-                      kg <input type="number" value={car.kg} />
+                    <button type="button" className="decrement">-</button>
+                    <h5 className="price">
+                       <input type="number" value={car.kg}  />kg
                     </h5>
-                    <button type="button">+</button>
+                    <button type="button" className="increment">+</button>
 
                     <input type="text" value={car.precio} />
-                    <div>
-                      <button type="button"><BsTrash /></button>
-                      <button type="button"><BsPencilSquare /></button>
+                    <div className="cont-btn">
+                      <button type="button" className="btn-trash">
+                        <BsTrash color="#b22222" size={"1.2rem"} />
+                      </button>
+                      <button type="button" className="btn-pencil">
+                        <BsPencilSquare color="#1e90ff" size={"1.2rem"} />
+                      </button>
                     </div>
                   </div>
                 </div>
               ))}
+            </div>
+            <div className="cont-send">
+              <h4>Total De la Compra</h4>
+              <input type="number" className="inp-total"/>
+              <div className="cont-btn-sens">
+                <button type="button">ENVIAR COMPRA</button>
+                <button type="button">VACIAR CARRO</button>
+              </div>
             </div>
           </div>
         </div>
